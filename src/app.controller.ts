7e6348581c4a23e58770f927715e4791097f50f2
Rayen Mabrouk <mabroukr911@gmail.com>
2024-03-15 15:20:15 +0100

    Add Public decorator to AppController methods

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,16 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './shared/decorators/public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+  @Public()
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
-
+  @Public()
   @Get('hello/:name')
   getHelloName(@Param('name') name: string): string {
     return this.appService.getHelloName(name);
